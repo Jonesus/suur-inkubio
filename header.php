116776f4cf647a3bb2b3d1ddc9b7d620e9629fa6
Jonesus <joonas.palosuo@aalto.fi>
2017-06-19 23:32:05 +0300

    Header looks like something

--- header.php
+++ header.php
@@ -8,14 +8,14 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Template for Bootstrap</title>
+    <title>Suur-Inkubio</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('template_directory');?>/assets/css/blog.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_directory');?>/assets/css/front.css" rel="stylesheet">
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -28,21 +28,23 @@
 
   <body>
 
-    <div class="blog-masthead">
-      <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
-        </nav>
+    <nav class="navbar-masthead navbar-fixed-top">
+      <div class="container-fluid text-center">
+        <a class="navbar-item active" href="#">Abeille</a>
+        <a class="navbar-item" href="#">Yrityksille</a>
+        <a class="navbar-item" href="#">Kiltalaisille</a>
+        <a class="navbar-item" href="#">Yhteystiedot</a>
       </div>
-    </div>
+
+      <div class="container-fluid" style="position:absolute; top:0;">
+        <a class="navbar-brand" href="#">
+          <img alt="Inkubio" height="20" src="<?php bloginfo('template_directory');?>/assets/images/inkulogo-valko.png">
+        </a>
+      </div>
+    </nav>
 
     <div class="container">
-
-      <div class="blog-header">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+      <div class="blog-header text-center">
+        <h1 class="blog-title">Suur-Inkubio</h1>
+        <p class="lead blog-description">Gotta get dat 75 fuksia</p>
       </div>
