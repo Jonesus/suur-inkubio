d65866808963ff2417474e867f26291d626c91e3
Maikki Inkubiitti <maikki@inkubaattori.kyla.fi>
2017-06-22 19:02:33 +0300

    Readjusted viewport

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=400, initial-scale=0.5">
+    <meta name="viewport" content="width=500, initial-scale=0.7">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
