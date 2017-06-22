3e7ea63653a2ae21b74b021ca5ad67d1ea8c377c
Jonesus <joonas.palosuo@aalto.fi>
2017-06-22 18:46:07 +0300

    Refactored image folder structure

--- content.php
+++ content.php
@@ -1,42 +1,38 @@
 <!-- First section -->
-<div class="page-section bg-image">
-  <div class="container-fluid">
+<div class="page-section bg-image" style="background-image: url(<?php bloginfo('template_directory');?>/assets/images/frontpage/etu.jpg);">
 
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="title-text text-center">
-          <h1 class="blog-title">Suur-Inkubio</h1>
-          <p class="lead ingress-text">Gotta get dat 75 fuksia</p>
-        </div>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="title-text text-center">
+        <h1 class="blog-title">Suur-Inkubio</h1>
+        <p class="lead ingress-text">Gotta get dat 75 fuksia</p>
       </div>
     </div>
-
   </div>
+
 </div>
 <!-- End first section -->
 
 <!-- Second section -->
 <div class="page-section">
-  <div class="container-fluid">
 
-    <div class="row row-content justify-content-center">
-      <div class="col-md-6 col-lg-4 align-self-center">
-        <p class="paragraph-text text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-      </div>
-      <div class="col-md-6 col-lg-4 align-self-center text-center">
-        <img class="rounded-circle img-fluid" alt="" src="<?php bloginfo('template_directory');?>/assets/images/genes.jpg">
-      </div>
+  <div class="row row-content justify-content-center">
+    <div class="col-md-6 col-lg-4 align-self-center">
+      <p class="paragraph-text text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
     </div>
-
-    <div class="row row-content justify-content-center flex-column-reverse flex-md-row">
-      <div class="col-md-6 col-lg-4 align-self-center text-center">
-        <img class="rounded-circle img-fluid" alt="" src="<?php bloginfo('template_directory');?>/assets/images/poutsi.jpg">
-      </div>
-      <div class="col-md-6 col-lg-4 align-self-center">
-        <p class="paragraph-text text-justify">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-      </div>
+    <div class="col-md-6 col-lg-4 align-self-center text-center">
+      <img class="rounded-circle img-fluid" alt="" src="<?php bloginfo('template_directory');?>/assets/images/frontpage/genes.jpg">
     </div>
-
   </div>
+
+  <div class="row row-content justify-content-center flex-column-reverse flex-md-row">
+    <div class="col-md-6 col-lg-4 align-self-center text-center">
+      <img class="rounded-circle img-fluid" alt="" src="<?php bloginfo('template_directory');?>/assets/images/frontpage/poutsi.jpg">
+    </div>
+    <div class="col-md-6 col-lg-4 align-self-center">
+      <p class="paragraph-text text-justify">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+  </div>
+
 </div>
 <!-- End second section -->
