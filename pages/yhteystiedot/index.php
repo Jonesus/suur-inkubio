5bf3bd840b3a42e8cc40b2af073e845fc3a37fec
Jonesus <joonas.palosuo@aalto.fi>
2017-06-23 18:30:10 +0300

    Initial implementation of contacts-subpage

---
+++ pages/yhteystiedot/index.php
@@ -0,0 +1,60 @@
+<!-- Necessary 'import' for all subpages -->
+<?php require('../../../../../wp-blog-header.php'); ?>
+
+
+<?php get_header(); ?>
+
+<?php add_template("image-row.php", ["rows" => [[["imgpath" => "hallitus/puheenjohtaja.jpg",
+                                                  "name" => "Tuomas Poutanen",
+                                                  "position" => "Puheenjohtaja",
+                                                  "email" => "tuomas.poutanen@aalto.fi"],
+                                                 ["imgpath" => "hallitus/tiedotusmestari.jpg",
+                                                  "name" => "Veera Itälinna",
+                                                  "position" => "Tiedotusmestari",
+                                                  "email" => "veera.italinna@aalto.fi"],
+                                                 ["imgpath" => "hallitus/nettisivuvastaava.jpg",
+                                                  "name" => "Joonas Palosuo",
+                                                  "position" => "Nettisivuvastaava",
+                                                  "email" => "joonas.palosuo@aalto.fi"]],
+
+                                                [["imgpath" => "hallitus/puheenjohtaja.jpg",
+                                                  "name" => "Tuomas Poutanen",
+                                                  "position" => "Puheenjohtaja",
+                                                  "email" => "tuomas.poutanen@aalto.fi"],
+                                                 ["imgpath" => "hallitus/tiedotusmestari.jpg",
+                                                  "name" => "Veera Itälinna",
+                                                  "position" => "Tiedotusmestari",
+                                                  "email" => "veera.italinna@aalto.fi"],
+                                                 ["imgpath" => "hallitus/tiedotusmestari.jpg",
+                                                  "name" => "Veera Itälinna",
+                                                  "position" => "Tiedotusmestari",
+                                                  "email" => "veera.italinna@aalto.fi"],
+                                                 ["imgpath" => "hallitus/nettisivuvastaava.jpg",
+                                                  "name" => "Joonas Palosuo",
+                                                  "position" => "Nettisivuvastaava",
+                                                  "email" => "joonas.palosuo@aalto.fi"]],
+
+                                                [["imgpath" => "hallitus/tiedotusmestari.jpg",
+                                                  "name" => "Veera Itälinna",
+                                                  "position" => "Tiedotusmestari",
+                                                  "email" => "veera.italinna@aalto.fi"],
+                                                 ["imgpath" => "hallitus/nettisivuvastaava.jpg",
+                                                  "name" => "Joonas Palosuo",
+                                                  "position" => "Nettisivuvastaava",
+                                                  "email" => "joonas.palosuo@aalto.fi"]],
+
+                                                [["imgpath" => "hallitus/puheenjohtaja.jpg",
+                                                  "name" => "Tuomas Poutanen",
+                                                  "position" => "Puheenjohtaja",
+                                                  "email" => "tuomas.poutanen@aalto.fi"]],
+
+
+
+                                                ]]); ?>
+
+
+
+
+
+
+<?php get_footer(); ?>
---
+++ template-parts/image-row.php
@@ -0,0 +1,57 @@
+<!-- Begin section -->
+<div class="page-section">
+
+  <?php $i = 0; foreach($args["rows"] as $row) { $i++; ?>
+
+    <div class="row justify-content-center">
+    <?php if (count($row) == 4) : 
+      foreach($row as $person) { ?>
+
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 text-center">
+          <img class="rounded-circle img-fluid" alt="" src="<?php echo $basedir . "/assets/images/" . $person["imgpath"]?>" >
+          <h3 class="text-center"><?php echo $person["name"]?></h3>
+          <h4 class="text-center"><?php echo $person["position"]?></h3>
+          <h5 class="text-center"><?php echo $person["email"]?></h3>
+          </br>
+      </div>
+
+    <?php } elseif (count($row) == 3) : 
+      foreach($row as $person) { ?>
+
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 text-center">
+          <img class="rounded-circle img-fluid" alt="" src="<?php echo $basedir . "/assets/images/" . $person["imgpath"]?>" >
+          <h3 class="text-center"><?php echo $person["name"]?></h3>
+          <h4 class="text-center"><?php echo $person["position"]?></h3>
+          <h5 class="text-center"><?php echo $person["email"]?></h3>
+          </br>
+      </div>
+
+    <?php } elseif (count($row) == 2) : 
+      foreach($row as $person) { ?>
+
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 text-center">
+          <img class="rounded-circle img-fluid" alt="" src="<?php echo $basedir . "/assets/images/" . $person["imgpath"]?>" >
+          <h3 class="text-center"><?php echo $person["name"]?></h3>
+          <h4 class="text-center"><?php echo $person["position"]?></h3>
+          <h5 class="text-center"><?php echo $person["email"]?></h3>
+          </br>
+      </div>
+
+    <?php } elseif (count($row) == 1) : 
+      $person = $row[0]; ?>
+
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 text-center">
+          <img class="rounded-circle img-fluid" alt="" src="<?php echo $basedir . "/assets/images/" . $person["imgpath"]?>" >
+          <h3 class="text-center"><?php echo $person["name"]?></h3>
+          <h4 class="text-center"><?php echo $person["position"]?></h3>
+          <h5 class="text-center"><?php echo $person["email"]?></h3>
+          </br>
+      </div>
+
+	  <?php endif; ?>
+    </div>
+  <?php } # endfor ?>
+
+
+</div>
+<!-- End section -->
