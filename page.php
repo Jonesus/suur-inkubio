10db66750265ccdbc8730b2bb4f5b4295338fdc2
Jonesus <joonas.palosuo@aalto.fi>
2017-06-25 22:27:11 +0300

    Rehaul of deep.inkubio pages

--- page.php
+++ page.php
@@ -2,35 +2,30 @@
 
 <?php get_sidebar(); ?>
 
-<div class="col-sm-9 offset-sm-3 col-md-10 offset-md-2">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/page.css">
+    <?php $page = get_post(); ?>
 
+    <div class="col-sm-9 offset-sm-3 col-md-10 offset-md-2">
 
+      <?php
+        $page_img = get_post_meta( get_the_ID(), 'header_img_path', true);
+        if (empty($page_img)) {
+          $page_img = "page-headers/amfi.jpg";
+        }
+      ?>
 
-<?php
-$page = get_post();
-$content = apply_filters('the_content', $page->post_content); 
-echo $content;
-$children = get_children($page->ID);
-foreach($children as $child) {
-    echo $child->post_name . "\n";
-}
-?>
+     <span class="page-bg-image" style="background-image: url(<?php bloginfo('template_directory') ?>/assets/images/<?php echo $page_img ?>);"></span>
+      <div class="row justify-content-center" style="background-color: rgb(240,240,240);">
+        <div class="col-sm-9 col-md-10 col-lg-8 col-xl-6 text-justify below-image">
 
-<?php add_template("image-title.php", ["bg-img" => "frontpage/kiva.jpg",
-                                       "h1" => "Kalja on hyvää",
-                                       "p" => "Kimble on erihuono peli",]); ?>
+          <h1 class="text-center"><?php echo get_the_title() ?></h1>
+          <?php echo apply_filters('the_content', $page->post_content); ?>
 
-<?php add_template("text-image.php", ["rows" => [ ["Firmat on kivoi", "frontpage/poutsi.jpg"],
-                                                  ["Antakaa meille fygyy", "frontpage/poutsi.jpg"],
-                                                  ["Muuten\nvedetää teit lättyy", "frontpage/genes.jpg"],
-                                                ]]); ?>
-
-
-
-</div>
-</div>
-
-</div>
+        </div>
+      </div>
+    </div> <!-- Content column -->
+  </div> <!-- Sidebar row -->
+</div> <!-- Sidebar container -->
 
 <?php get_footer(); ?>
 
