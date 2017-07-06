9bbe9bec12f3a5863cea17ccbedff8fc2381e296
Jonesus <joonas.palosuo@aalto.fi>
2017-07-06 21:53:49 +0300

    More inkubio-font

--- template-parts/image-title.php
+++ template-parts/image-title.php
@@ -6,7 +6,7 @@
 
   <div class="row padded-row">
     <div class="col-lg-12 title-text text-center">
-      <h1> <?php echo $args["h1"] ?> </h1>
+      <h1 style="font-size: 8em;"> <?php echo $args["h1"] ?> </h1>
       <p class="ingress-text"> <?php echo $args["p"]?> </p>
     </div>
   </div>
