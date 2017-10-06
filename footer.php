62f32232919545737b197e3f6ebfb5b37cb68481
Jonesus <joonas.palosuo@aalto.fi>
2017-10-06 21:48:45 +0300

    Reworked footer

--- footer.php
+++ footer.php
@@ -1,8 +1,23 @@
 
   <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/footer.css">
   <footer class="footer">
-    <p>&copy; Inkubio ry</p>
-    <p>All rights arrested</p>
+    <div class="d-flex align-items-center h100 justify-content-center">
+      <img src="<?php bloginfo('template_directory');?>/assets/images/logos/futurice.png">
+      <img src="<?php bloginfo('template_directory');?>/assets/images/logos/mectalent.png">
+      <img src="<?php bloginfo('template_directory');?>/assets/images/logos/roche.png">
+      <img src="<?php bloginfo('template_directory');?>/assets/images/logos/thermo.png">
+    </div>
+    <div class="d-flex justify-content-between" style="padding-top: 30px;">
+      <div style="padding-left: 6vw;">
+        Inkubio ry 2017
+      </div>
+      <div style="padding-right: 6vw;">
+        <a href="">Facebook</a>
+        <a href="">Twitter</a>
+        <a href="">Instagram</a>
+        <a href="">LinkedIn</a>
+      </div>
+    </div>
   </footer>
 
 
