e0b9df86fbe6df6044e14f16c8bcbf242411234c
Jonesus <joonas.palosuo@aalto.fi>
2017-10-07 18:27:02 +0300

    Pill buttons and navbar underline animation

--- index.php
+++ index.php
@@ -14,8 +14,14 @@
 <!-- Begin section -->
 <div class="page-section">
   <div class="twist upper">
-    <div class="row twist-content justify-content-center">
 
+    <div class="row twist-content justify-content-center">
+      <div class="title">
+        Sample text
+      </div>
+    </div>
+    
+    <div class="row twist-content justify-content-center">
       <div class="col-sm-12 col-md-6 col-xl-4">
         <p class="paragraph-text">
           It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
@@ -26,8 +32,17 @@
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
       </div>
-
     </div>
+
+    <div class="row twist-content justify-content-center">
+      <div class="col-sm-12 col-md-6 col-xl-4 text-center">
+        <a href="" class="pill-button">Dank</a>
+      </div>
+      <div class="col-sm-12 col-md-6 col-xl-4 text-center">
+        <a href="" class="pill-button">Meme</a>
+      </div>
+    </div>
+
   </div>
 </div>
 <!-- End section -->
