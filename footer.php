c438064185c515fe11788b315ffd2e49dd4855c5
jonesus <joonas.palosuo@aalto.fi>
2017-10-08 01:23:01 +0300

    Replaced logo bitmaps with vectors

--- footer.php
+++ footer.php
@@ -2,17 +2,25 @@
   <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/footer.css">
   <footer class="footer upper">
     <div class="row justify-content-center">
-      <div class="col-sm-6 col-md-3 align-items-center">
-        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/futurice.png">
+      <div class="col-sm-10 col-md-8 col-lg-6 col-xl-4 align-items-center" style="border-bottom: 2px solid; margin-bottom: 20px;">
+        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/biopartners.svg">
       </div>
-      <div class="col-sm-6 col-md-3 align-items-center">
-        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/mectalent.png">
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
+        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/futurice.svg">
       </div>
-      <div class="col-sm-6 col-md-3 align-items-center">
-        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/roche.png">
+      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
+        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/mectalent.svg">
       </div>
-      <div class="col-sm-6 col-md-3 align-items-center">
-        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/thermo.png">
+      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
+        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/roche.svg">
+      </div>
+      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
+        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/thermo.svg">
+      </div>
+      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
+        <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/ge.svg">
       </div>
     </div>
     <div class="d-flex justify-content-between" style="padding-top: 30px;">
