0dd4a405864dc14e400ebbd10dbef59581a77312
jonesus <joonas.palosuo@aalto.fi>
2017-10-08 16:52:41 +0300

    Card-styled ui for contacts-page

--- template-parts/image-row.php
+++ template-parts/image-row.php
@@ -3,27 +3,29 @@
 
 <?php $i = 0; foreach($args["rows"] as $row) { $i++; ?>
 
-  <div class="row justify-content-center">
+  <div class="row row-fix justify-content-center">
   <?php if (count($row) == 3) : 
     foreach($row as $person) { ?>
 
     <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 text-center">
-      <img class="rounded-circle img-fluid" alt="" src="<?php echo $basedir . "/assets/images/" . $person["imgpath"]?>" >
-      <h3 class="text-center"><?php echo $person["name"]?></h3>
-      <h4 class="text-center"><?php echo $person["position"]?></h3>
-      <h5 class="text-center"><?php echo $person["email"]?></h3>
-      </br>
+      <div class="card">
+        <img class="rounded-circle img-fluid" alt="" src="<?php echo $basedir . "/assets/images/" . $person["imgpath"]?>" >
+        <h3 class="text-center"><?php echo $person["name"]?></h3>
+        <h4 class="text-center"><?php echo $person["position"]?></h3>
+        <h5 class="text-center"><?php echo $person["email"]?></h3>
+      </div>
     </div>
 
   <?php } else : # 1, 2 or 4 circles
     foreach($row as $person) { ?>
 
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 text-center">
-      <img class="rounded-circle img-fluid" alt="" src="<?php echo $basedir . "/assets/images/" . $person["imgpath"]?>" >
-      <h3 class="text-center"><?php echo $person["name"]?></h3>
-      <h4 class="text-center"><?php echo $person["position"]?></h3>
-      <h5 class="text-center"><?php echo $person["email"]?></h3>
-      </br>
+      <div class="card">
+        <img class="rounded-circle img-fluid" alt="" src="<?php echo $basedir . "/assets/images/" . $person["imgpath"]?>" >
+        <h3 class="text-center"><?php echo $person["name"]?></h3>
+        <h4 class="text-center"><?php echo $person["position"]?></h3>
+        <h5 class="text-center"><?php echo $person["email"]?></h3>
+      </div>
     </div>
 
   <?php } endif; ?>
