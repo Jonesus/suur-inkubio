567d2d6187fd8ceb9967308f26bddb9611da4531
jonesus <joonas.palosuo@aalto.fi>
2017-10-08 13:35:31 +0300

    Reorder of sponsors

--- footer.php
+++ footer.php
@@ -1,25 +1,17 @@
 
   <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/footer.css">
   <footer class="footer upper">
+
     <div class="row justify-content-center">
       <div class="col-sm-10 col-md-8 col-lg-6 col-xl-4 align-items-center" style="border-bottom: 2px solid; margin-bottom: 20px;">
         <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/biopartners.svg">
       </div>
     </div>
+
     <div class="row justify-content-center">
-      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
-        <a href="http://futurice.com/">
-          <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/futurice.svg">
-        </a>
-      </div>
       <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
         <a href="http://www.mectalent.fi/">
           <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/mectalent.svg">
-        </a>
-      </div>
-      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
-        <a href="https://www.roche.com/">
-          <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/roche.svg">
         </a>
       </div>
       <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
@@ -28,11 +20,22 @@
         </a>
       </div>
       <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
+        <a href="https://www.roche.com/">
+          <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/roche.svg">
+        </a>
+      </div>
+      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
+        <a href="http://futurice.com/">
+          <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/futurice.svg">
+        </a>
+      </div>
+      <div class="col-sm-6 col-lg-4 col-xl-2 align-items-center">
         <a href="http://www.gehealthcare.fi/">
           <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/logos/ge.svg">
         </a>
       </div>
     </div>
+
     <div class="d-flex justify-content-between" style="padding-top: 30px;">
       <div style="padding-left: 6vw;">
         Inkubio ry 2017
@@ -44,8 +47,8 @@
         <a href="">LinkedIn</a>
       </div>
     </div>
-  </footer>
 
+  </footer>
 
   <!-- Bootstrap core JavaScript
   ================================================== -->
