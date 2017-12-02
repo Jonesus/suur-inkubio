0ce0bb1a901bc96e0c9060b68884e3f81a99c869
Jonesus <joonas.palosuo@aalto.fi>
2017-12-03 00:01:44 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+  <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/page.css">
+  <div class="background">
+    <div class="row justify-content-center row-fix">
+      <div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 content-page">
+
+        <div class="error404 text-center">404</div>
+        <img style="width:70%" class="rotate180 center" alt="" src="<?php bloginfo('template_directory');?>/assets/images/inkulogo-viher.svg">
+        <h3 style="margin-top: 50px;" class="text-center"><?php _e('Oho, jotain meni vikaan.') ?></h3>
+
+      </div>
+    </div>
+  </div>
+
+<?php get_footer(); ?>
