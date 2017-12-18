2bd7829ba923258f513bfb7584ecf2dfad6b4645
Oskari <oskari.lehtonen@aalto.fi>
2017-12-18 13:40:07 +0200

    registered menus and widgets to wordpress. Under Appearance tab

--- functions.php
+++ functions.php
@@ -1,5 +1,29 @@
 <?php
 
+//register nav-menus to wordpress
+function suurinkubio_setup() {
+    register_nav_menus( array(
+        'primary' => __('Primary Menu', 'suurinkubio') 
+    ));
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'suurinkubio_setup');
+
+//register widgets to wordprss
+function suurinkubio_widgets() {
+    register_sidebar( array(
+        'name'          => __( 'Sidebar', 'suurinkubio' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Add widgets here to appear in the sidebar', 'suurinkubio' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>'
+    ) );
+}
+add_action( 'widgets_init' , 'suurinkubio_widgets' );
+
+
 function add_template($filename, $arguments) {
     # Horrible hack to get path from echoed output
     ob_start();
