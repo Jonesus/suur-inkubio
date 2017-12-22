88830cfff0eea76f68ce15857298c77dd2296b38
Jonesus <joonas.palosuo@aalto.fi>
2017-12-23 01:56:12 +0200

    Add blog pages

---
+++ single.php
@@ -0,0 +1,45 @@
+<?php get_header("deep"); ?>
+
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/page.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/post.css">
+    <?php $post = get_post(); ?>
+
+    <div class="col-sm-12 col-md-9 offset-md-3 col-xl-10 offset-xl-2 ml-auto background">
+      <div class="row content-container justify-content-center">
+
+        <div id="page" class="col-sm-11 col-md-11 col-lg-9 col-xl-9 content-page">
+          <h1 class="text-center"><?php echo get_the_title() ?></h1>
+          <?php echo apply_filters('the_content', $post->post_content); ?>
+        </div>
+
+        <div class="col-sm-11 col-md-10 col-lg-8 col-xl-6">
+          <div class="row justify-content-between">
+
+            <?php $posts = query_posts($query_string); if (have_posts()) : while (have_posts()) : the_post(); ?>
+              <div class="post-nav-container col-5">
+                <?php if (get_adjacent_post(false, '', true)): ?>
+                  <div class="post-nav post-link align-middle">
+                    <?php previous_post_link(); ?>
+                  </div>
+                <?php endif; ?>
+              </div>
+              <div class="post-nav-container col-5">
+                <?php if (get_adjacent_post(false, '', false)): ?>
+                  <div class="post-nav post-link align-middle">
+                    <?php next_post_link(); ?>
+                  </div>
+                <?php endif; ?>
+              </div>
+            <?php endwhile; endif; ?>
+
+          </div>
+        </div>
+
+      </div>
+    </div> <!-- Content column -->
+
+  </div> <!-- Sidebar row -->
+</div> <!-- Sidebar container -->
+
+<?php get_footer(); ?>
+
