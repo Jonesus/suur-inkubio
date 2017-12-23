476164ebd50abbb1f85ec9d1078ef046740ebd53
Jonesus <joonas.palosuo@aalto.fi>
2017-12-23 15:21:22 +0200

    Fix 404 layout

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ get_header("deep"); ?>
   <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/page.css">
   <div class="background">
     <div class="row justify-content-center row-fix">
-      <div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 content-page">
+      <div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 content-page content-container">
 
         <div class="error404 text-center">404</div>
         <img style="width:70%" class="rotate180 center" alt="" src="<?php bloginfo('template_directory');?>/assets/images/inkulogo-viher.svg">
