55b486c9930014fdb6e7803694f117bde5416e54
Jonesus <joonas.palosuo@aalto.fi>
2017-12-29 14:30:31 +0200

    Add post date and author

--- single.php
+++ single.php
@@ -4,18 +4,21 @@
     <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/post.css">
     <?php $post = get_post(); ?>
 
+    <?php $posts = query_posts($query_string); if (have_posts()) : while (have_posts()) : the_post(); ?>
     <div class="col-sm-12 col-md-9 offset-md-3 col-xl-10 offset-xl-2 ml-auto background">
       <div class="row content-container justify-content-center">
 
         <div id="page" class="col-sm-11 col-md-11 col-lg-9 col-xl-9 content-page">
           <h1 class="text-center"><?php echo get_the_title() ?></h1>
+          <div class="post-info">
+            <?php echo get_the_date('d.m.Y') . ' ' . get_the_author(); ?>
+          </div>
           <?php echo apply_filters('the_content', $post->post_content); ?>
         </div>
 
         <div class="col-sm-11 col-md-10 col-lg-8 col-xl-6">
           <div class="row justify-content-between">
 
-            <?php $posts = query_posts($query_string); if (have_posts()) : while (have_posts()) : the_post(); ?>
               <div class="post-nav-container col-5">
                 <?php if (get_adjacent_post(false, '', true)): ?>
                   <div class="post-nav post-link align-middle">
@@ -30,13 +33,13 @@
                   </div>
                 <?php endif; ?>
               </div>
-            <?php endwhile; endif; ?>
 
           </div>
         </div>
 
       </div>
     </div> <!-- Content column -->
+    <?php endwhile; endif; ?>
 
   </div> <!-- Sidebar row -->
 </div> <!-- Sidebar container -->
