a6144a20a13e3ce447a15950f47e35405af1f3ef
Jonesus <joonas.palosuo@aalto.fi>
2017-12-31 14:39:51 +0200

    Add option to hide title

--- template-parts/deep-page-structure.php
+++ template-parts/deep-page-structure.php
@@ -1,3 +1,9 @@
+<?php
+  $page = get_post();
+  $meta = get_post_meta($page->ID);
+?>
+
+
 <?php get_header("deep"); ?>
 
     <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/page.css">
@@ -6,8 +12,10 @@
     <div class="col-sm-12 col-md-9 offset-md-3 col-xl-10 offset-xl-2 ml-auto background">
       <div class="row content-container justify-content-center">
         <div id="page" class="col-sm-11 col-md-11 col-lg-9 col-xl-9 content-page">
-
-          <h1><?php echo get_the_title() ?></h1>
+          
+          <?php if ($meta['hide_title'][0] != 'true') : ?>
+            <h1><?php echo get_the_title() ?></h1>
+          <?php endif; ?>
           <?php echo apply_filters('the_content', $page->post_content); ?>
 
           <?php if (isset($args['template'])) {
